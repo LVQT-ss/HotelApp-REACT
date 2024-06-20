5a73b0d262fc9273ef1a1076521996a6eaa83f2d
LVQT <levietquocthinh@gmail.com>
2024-06-20 15:15:20 +0700

    add filter room and fix bug

--- lakeSide-hotel/src/App.jsx
+++ lakeSide-hotel/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from "./Component/room/AddRoom.jsx";
+import ExistingRooms from "./Component/room/ExistingRooms.jsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <AddRoom/>
+     <ExistingRooms/>
     </>
   )
 }
