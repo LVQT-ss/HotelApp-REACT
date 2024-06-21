2114171db2cfe95d35f8ea86c0873fc991997598
LVQT <levietquocthinh@gmail.com>
2024-06-22 00:00:12 +0700

    added room carousel item

--- lakeSide-hotel/src/Component/home/Home.jsx
+++ lakeSide-hotel/src/Component/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainHeader from "../layout/MainHeader";
 import HotelService from "../common/HotelService";
 import Parallax from "../common/Parallax";
+import RoomCarousel from "../common/RoomCarousel";
 
 const Home = () => {
   return (
@@ -9,9 +10,12 @@ const Home = () => {
       <MainHeader />
 
       <section className="container">
+        <RoomCarousel/>
         <Parallax />
+        <RoomCarousel/>
         <HotelService />
         <Parallax />
+        <RoomCarousel/>
       </section>
     </section>
   );
