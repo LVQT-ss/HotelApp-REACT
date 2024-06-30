a27a16099174aad6d31524915ebebbf709762618
LVQT <levietquocthinh@gmail.com>
2024-06-30 23:23:49 +0700

    handle Date

--- lakeSide-hotel/src/Component/common/DateSlider.jsx
+++ lakeSide-hotel/src/Component/common/DateSlider.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "react-date-range/dist/styles.css"
+import "react-date-range/dist/theme/default.css"
+import { DateRangePicker } from 'react-date-range'
+
 
 const DateSlider = () => {
+    const [dateRange,setDateRange] = useState({
+        startDate: undefined,
+        endDate: undefined,
+        key: 'selection',
+  
+    })
+    const handleSelect = (ranges) => {
+		setDateRange(ranges.selection)
+		onDateChange(ranges.selection.startDate, ranges.selection.endDate)
+		onFilterChange(ranges.selection.startDate, ranges.selection.endDate)
+	}
+
+	const handleClearFilter = () => {
+		setDateRange({
+			startDate: undefined,
+			endDate: undefined,
+			key: "selection"
+		})
+		onDateChange(null, null)
+		onFilterChange(null, null)
+	}
   return (
     <div>
       
