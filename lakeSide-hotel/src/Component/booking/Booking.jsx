b713e95347bec9366191635fa56b97ea4bda3de2
LVQT <levietquocthinh@gmail.com>
2024-07-31 00:42:21 +0700

    add br/

--- lakeSide-hotel/src/Component/booking/Booking.jsx
+++ lakeSide-hotel/src/Component/booking/Booking.jsx
@@ -37,7 +37,7 @@ const Booking = () => {
 
 
   return (
-    <section className='container' style={{backgroundColor:"whitesmoke"}}>
+    <section style={{backgroundColor:"whitesmoke"}}>
       <Header title={"existing Bookings"}/>
       {error & (<div className='text-danger'>{error}</div>)}
       {isLoading ? (<div>
