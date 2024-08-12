b95820d50393a8c8931b53f892a0ce96328c0c52
LVQT <levietquocthinh@gmail.com>
2024-08-12 22:08:40 +0700

    add totken and User profile

--- lakeSide-hotel/src/Component/Utils/ApiFunctions.js
+++ lakeSide-hotel/src/Component/Utils/ApiFunctions.js
@@ -4,6 +4,15 @@ export const api = axios.create({
     baseURL: "http://localhost:8081"
 })
 
+
+export const getHeader = () => {
+	const token = localStorage.getItem("token")
+	return {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json"
+	}
+}
+
     // this function add new room
 export async function addRoom(photo,roomType,roomPrice){
     const formData = new FormData()
@@ -164,5 +173,41 @@ export async function loginUser(login) {
 	} catch (error) {
 		console.error(error)
 		return null
+	}
+}
+
+/*  This is function to get the user profile */
+export async function getUserProfile(userId, token) {
+	try {
+		const response = await api.get(`users/profile/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		throw error
+	}
+}
+
+/* This isthe function to delete a user */
+export async function deleteUser(userId) {
+	try {
+		const response = await api.delete(`/users/delete/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		return error.message
+	}
+}
+
+/* This is the function to get a single user */
+export async function getUser(userId, token) {
+	try {
+		const response = await api.get(`/users/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		throw error
 	}
 }
